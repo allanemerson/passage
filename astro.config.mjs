2905b526188c0e71eab773c4cf4452210df86eca
Allan Emerson <allan@electricpulp.com>
2026-07-17 15:16:25 -0500

    adds wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://passage.emerson-allan.workers.dev',
     vite: {
         css: {
             preprocessorOptions: {
