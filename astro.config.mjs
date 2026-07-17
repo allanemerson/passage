41756cb2983b66350ed2715ddf9ee73e0696b91d
Allan Emerson <allan@electricpulp.com>
2026-07-17 15:00:09 -0500

    home and faq restored

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,66 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    loadPaths: ['src/styles'],
+                    additionalData: `@use "utils" as utils;`,
+                },
+            },
+        },
+    },
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: "JosefinSansRegular",
+            cssVariable: "--ff-josefin-sans--regular",
+            options: {
+                variants: [{
+                    src: ['./src/assets/fonts/JosefinSans-Regular.ttf'],
+                    weight: 'normal',
+                    style: 'normal'
+                }]
+            }
+        },
+        {
+            provider: fontProviders.local(),
+            name: "JosefinSansItalic",
+            cssVariable: "--ff-josefin-sans--italic",
+            options: {
+                variants: [{
+                    src: ['./src/assets/fonts/JosefinSans-Italic.ttf'],
+                    weight: 'normal',
+                    style: 'normal'
+                }]
+            }
+        },
+        {
+            provider: fontProviders.local(),
+            name: "JosefinSansBold",
+            cssVariable: "--ff-josefin-sans--bold",
+            options: {
+                variants: [{
+                    src: ['./src/assets/fonts/JosefinSans-Bold.ttf'],
+                    weight: 'normal',
+                    style: 'normal'
+                }]
+            }
+        },
+        {
+            provider: fontProviders.local(),
+            name: "Manchester",
+            cssVariable: "--ff-manchester--regular",
+            options: {
+                variants: [{
+                    src: ['./src/assets/fonts/ManchesterSignature.ttf'],
+                    weight: 'normal',
+                    style: 'normal'
+                }]
+            }
+        },
+    ]
+});
---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const TICKET_LINK = "https://shotgun.live/en/festivals/passage-gathering-2026-to-the-woods";
+export const FACEBOOK_URL = "https://www.facebook.com/events/1003468572651863/";
+export const DISCORD_URL = "https://discord.gg/aEV8mkGN";
+export const EASTERN_TZ = "America/New_York";
