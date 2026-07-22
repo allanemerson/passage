4da9b790857f8236bbde1dfd866b9dbf3b9caabd
Allan Emerson <allan@electricpulp.com>
2026-07-21 22:03:54 -0500

    moves property to standalone page

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const TICKET_LINK = "https://shotgun.live/en/festivals/passage-gathering-2026-to-the-woods";
 export const FACEBOOK_URL = "https://www.facebook.com/events/1003468572651863/";
-export const DISCORD_URL = "https://discord.gg/aEV8mkGN";
+export const DISCORD_URL = "https://discord.gg/qBVjsTx7e";
 export const EASTERN_TZ = "America/New_York";
+export const HERO_ALT = "Passage Gathering 2026, September 18 - 20 | Farmcamp"
